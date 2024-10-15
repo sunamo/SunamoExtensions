8a7166fd8902426ff56a272c14050cfaed4286d6
sunamo.cz <radek.jancik@sunamo.cz>
2024-10-15 16:09:29 +0200

    Before commented values classes

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,12 +1,11 @@
-
 global using System.Collections.Generic;
 global using System.Collections;
 global using System.Linq;
 global using System.Text;
-global using System.Threading.Tasks;
 global using System;
-
 global using System.Diagnostics;
 global using System.Net;
 global using System.Reflection;
+global using System.Diagnostics.CodeAnalysis;
 global using SunamoExceptions;
+global using SunamoExceptions._sunamo.SunamoValues;
