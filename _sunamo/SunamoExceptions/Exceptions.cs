21a4c7b83d610f43533053ff2a531d2b3201ec66
sunamo.cz <radek.jancik@sunamo.cz>
2025-01-29 21:01:00 +0100

    Buildable, removed unused internal,private,protected methods in _sunamo

--- _sunamo/SunamoExceptions/Exceptions.cs
+++ _sunamo/SunamoExceptions/Exceptions.cs
@@ -3,10 +3,6 @@ namespace SunamoExtensions._sunamo.SunamoExceptions;
 internal sealed partial class Exceptions
 {
     #region Other
-    internal static string CheckBefore(string before)
-    {
-        return string.IsNullOrWhiteSpace(before) ? string.Empty : before + ": ";
-    }
 
     internal static string TextOfExceptions(Exception ex, bool alsoInner = true)
     {
@@ -24,32 +20,11 @@ internal sealed partial class Exceptions
         return r;
     }
 
-    internal static void TypeAndMethodName(string l, out string type, out string methodName)
-    {
-        var s2 = l.Split("at ")[1].Trim();
-        var s = s2.Split("(")[0];
-        var p = s.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries).ToList();
-        methodName = p[^1];
-        p.RemoveAt(p.Count - 1);
-        type = string.Join(".", p);
-    }
     #endregion
 
     #region IsNullOrWhitespace
     readonly static StringBuilder sbAdditionalInfoInner = new();
     readonly static StringBuilder sbAdditionalInfo = new();
-    internal static string AddParams()
-    {
-        sbAdditionalInfo.Insert(0, Environment.NewLine);
-        sbAdditionalInfo.Insert(0, "Outer:");
-        sbAdditionalInfo.Insert(0, Environment.NewLine);
-        sbAdditionalInfoInner.Insert(0, Environment.NewLine);
-        sbAdditionalInfoInner.Insert(0, "Inner:");
-        sbAdditionalInfoInner.Insert(0, Environment.NewLine);
-        var addParams = sbAdditionalInfo.ToString();
-        var addParamsInner = sbAdditionalInfoInner.ToString();
-        return addParams + addParamsInner;
-    }
     #endregion
 
     #region OnlyReturnString 
